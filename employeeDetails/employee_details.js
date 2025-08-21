427f2c86cd1cf23ea88a89271114c6df89bed514
Valentin Munteanu <valentinmunteanule@gmail.com>
2025-08-21 14:30:43 +0300

    mesaj2

--- employeeDetails/employee_details.js
+++ employeeDetails/employee_details.js
@@ -1,7 +1,7 @@
 const employees = [
-    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
-    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
-    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
+    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000, specialization: 'JavaScript' },
+    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000, specialization: 'Recruitment' },
+    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 6000, specialization: 'Accounting' }
     //... More employee records can be added here
 ];
 // Function to display all employees
@@ -23,5 +23,14 @@ function findEmployeeById(employeeId) {
     }
     else {
         document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this ID';
+    }
+}
+function findEmployeeSpecialization(employeeSpecialization) {
+    const foundEmployee = employees.find(employee => employee.specialization === employeeSpecialization);
+    if (foundEmployee) {
+        document.getElementById('employeesDetails').innerHTML = `<p>${foundEmployee.id}: ${foundEmployee.name}: ${foundEmployee.name} - ${foundEmployee.department} </p>`;
+    }
+    else {
+        document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this specialization';
     }
 }
